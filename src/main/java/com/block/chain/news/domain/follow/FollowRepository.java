ea70e0b952983ae748699a7183b219abd8e8ae6a
wooou <dksdnwls@gmail.com>
2020-06-07 21:45:52 +0900

    fix: follow 기능 및 unfollow 기능 버그 픽스

--- src/main/java/com/block/chain/news/domain/follow/FollowRepository.java
+++ src/main/java/com/block/chain/news/domain/follow/FollowRepository.java
@@ -2,9 +2,12 @@ package com.block.chain.news.domain.follow;
 
 import com.block.chain.news.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByFromUserAndToUser(User fromUser, User toUser);
+
+    void deleteByFollowId(Long followId);
 }
